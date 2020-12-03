8cc956d3d80c4a333bfba7e730be9f04abfc5a09
Quinton Fults <quinton.fults.work@gmail.com>
2020-12-03 13:20:05 -0800

    code

---
+++ routes/movieRoutes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router()
+
+router.get('/movies', (req, res) => {
+
+})
+
+router.post('/movies', (req, res) => {
+
+})
+
+router.put('/movies/:id', (req, res) => {
+
+})
+
+router.delete('/movies/:id', (req, res) => {
+  
+})
+
+module.exports = router
